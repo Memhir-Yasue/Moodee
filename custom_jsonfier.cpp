75f72f399ad10c95a4d8fd6f75efd5dd19bef41c
Memhir-Yasue <habesha787@gmail.com>
2019-03-06 23:34:28 -0500

    prototype for upcoming jsonfy class

---
+++ custom_jsonfier.cpp
@@ -0,0 +1,62 @@
+// #include "moodee.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// void jsonify()
+// {
+//   std::cout << " \"{\" " << date << ":[" '\n';
+//   std::cout << "  { \"" << mod <<", \"" << ":" << "\"\""
+// }
+
+std::string NAME = "Josh";
+
+std::string MOOD = "Mood";
+int m_mood = 3;
+
+std::string SLEEP = "Sleep";
+double m_sleep = 8.25;
+
+std::string FOOD = "Food";
+bool m_food = true;
+
+void print_jsonfy()
+{
+  std::cout << "{\n";
+  std::cout << "  \""<<NAME<<"\": [ \n";
+
+  std::cout << "    {\n";
+
+  std::cout << "      \""<<MOOD<<"\": \""<<m_mood<<"\", \n";
+  std::cout << "      \""<<SLEEP<<"\": \""<<m_sleep<<"\", \n";
+  std::cout << "      \""<<FOOD<<"\": \""<<m_food<<"\" \n";
+
+  std::cout << "    }\n";
+  std::cout << "  ]\n";
+  std::cout << "}, \n";
+}
+
+
+void write_jsonfy()
+{
+  std::ofstream outjson ("json.txt");
+  outjson << "{\n";
+  outjson << "  \""<<NAME<<"\": [ \n";
+
+  outjson << "    {\n";
+
+  outjson << "      \""<<MOOD<<"\": \""<<m_mood<<"\", \n";
+  outjson << "      \""<<SLEEP<<"\": \""<<m_sleep<<"\", \n";
+  outjson << "      \""<<FOOD<<"\": \""<<m_food<<"\" \n";
+
+  outjson << "    }\n";
+  outjson << "  ]\n";
+  outjson << "}, \n";
+}
+
+//
+int main(int argc, char const *argv[]) {
+  write_jsonfy();
+  print_jsonfy();
+  return 0;
+}
