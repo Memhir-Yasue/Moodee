be3ad1275172cf473042b90b99da9bfe9a0108f7
Memhir-Yasue <habesha787@gmail.com>
2019-03-07 13:51:03 -0500

    Template so far working

--- ioBox/lab.cpp
+++ ioBox/lab.cpp
@@ -5,11 +5,6 @@
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
-// void jsonfy()
-// {
-//
-// }
-
 void write(json obj) {
   std::ofstream w_file ("json.txt");
   w_file << obj;
@@ -21,65 +16,15 @@ void print(json obj) {
 
 int main()
 {
-  // json j2;
-  // j2["list"] = {}
-  // print(j2);
-  // j2.push_back("YasuE");
-  // // write(j2);
-  // print(j2);
-  // j2.push_back("saray");
-  // // write(j2);
-  // print(j2);
 
   /*
-  json j2 = {
-  {"pi", 3.141},
-  {"happy", true},
-  {"name", "Niels"},
-  {"nothing", nullptr},
-  {"answer", {
-    {"everything", 42}
-  }},
-  {"list", {1, 0, 2}},
-  {"object", {
-    {"currency", "USD"},
-    {"value", 42.99}
-  }}
-};
+    The stuff down below is working.
+    Testing by having two differente user-names as well as differente days of the month
+
+    CAN BE PARSED IN PYTHON!
+
   */
-
-
-  json j2 = {
-  {"pi", 3.141},
-  {"happy", true},
-  {"name", "Niels"},
-  {"nothing", nullptr},
-  {"answer", {
-    {"everything", 42}
-  }},
-
-  {
-    "list",
-      {
-        {
-          {
-            {
-              "currency", "USD"
-            },
-            {
-              "value", 42.99
-            }
-          }
-        }
-      }
-    }
-  };
-print(j2);
-write(j2);
-
   json j;
-  // j["user"] = {{"YasuE","year"}, {"Sare","year"} };
-  // j["user"]["year"] = {2017,2018,2019};
 
   for (int i = 1; i <= 31; i++) {
     std::string day = std::to_string(i);
@@ -91,21 +36,13 @@ write(j2);
                                                   };
   }
 
-  // j["users"]["YasuE"]["2018"]["January"] = {{"q1","user_input1"},{"q2","user_input1"}};
-  // j["users"]["YasuE"]["2018"]["January"] = {{"day2","user_input1"}};
-  j["data"]["users"]["YasuE"]["2019"]["Feburary"][5] = { {"Question1","user_input"},
+
+  j["data"]["users"]["SARA"]["2019"]["Feburary"][5] = { {"Question1","user_input"},
                                                   {"Question2","user_input"},
                                                   {"Question3","user_input"},
                                                   {"Question4","user_input"},
                                                   {"Question5","user_input"},
                                                 };
-
-  // j["user"].push_back("foo");
-  // j["user"].push_back(1);
-  // j["user"].push_back(true);
-
-  // std::cout << j <<"\n";
-  // write(j);
-
+  write(j);
   return 0;
 }
--- ioBox/parsejson.py
+++ ioBox/parsejson.py
@@ -4,8 +4,12 @@ import json
 raw_json = open('json.txt')
 
 
-json_ = json.load(raw_json)
+file = json.load(raw_json)
 
-currency = json_['list'][0][0]['currency']
+# currency = file['list'][0][0]['currency'] # for jsonCPY.txt
 
-print(currency)
+data = file['data']['users']['YasuE']['2018']['January']['12']
+users = file['data']['users'].keys()
+
+print(users)
+print(data)
