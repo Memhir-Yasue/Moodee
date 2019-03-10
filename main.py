3edfb3cc2b85352169a67afcdbf94c25bfe63fe2
Memhir-Yasue <habesha787@gmail.com>
2019-03-09 19:02:12 -0500

    Basic DB implemented!

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import moodee
+import dbmkr
+
+
+
 
 
 # print(dir(moodee))
 
-me_obj = moodee.Moodee()
-name = me_obj.name("YasuE")
-print(name)
+user = moodee.Moodee()
+
+user.setTime()
+name = user.name("YasuE")
+mood = user.mood(3)
+breakfast = user.breakfast(1)
+sbat = user.S_bat(0)
+bible = user.rd_bible(1)
+
+dbmkr.connect()
+dbmkr.dynamic_data_entry(name,mood,breakfast,sbat,bible)
+
+user.out("inputs.txt")
